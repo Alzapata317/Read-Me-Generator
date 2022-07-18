993d2a90b0588cb3b1e217ad15307465e3feea40
Alejandro Zapata <alzapata317@gmail.com>
2022-07-18 00:17:32 -0400

    added a prompt for users to input answers that will be converted into a ReadME file

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
+
+const generateMD = ({
+    title, 
+    description, 
+    installationInstructions, 
+    usageInformation, 
+    contributionGuidelines, 
+    testInstructions, 
+    license, 
+    githubUsername, 
+    email,
+})
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: "What do you want the title of your ReadME to be?",
+            validate: value =>{if (value){return true} else {return 'I need a value to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of your project',
+            validate: value =>{if (value){return true} else {return 'I need a value to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'installationInstructions',
+            message: 'Provide instructions in order to install this application',
+            validate: value =>{if (value){return true} else {return 'I need a value to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'usageInformation',
+            message: 'Provide information in which manner this application will be used',
+            validate: value =>{if (value){return true} else {return 'I need a value to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'contributionGuidelines',
+            message: 'Set up guidelines for how to contribue to the application',
+            validate: value =>{if (value){return true} else {return 'I need a value to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'testInstructions',
+            message: 'Provide instructions for how to test the application',
+            validate: value => {if(value){return true} else{return 'I need a value to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'Input your github username',
+            validate: value => {if (value){return true} else{return 'I need a value to continue'}}
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Input your email address',
+            validate: value => {if (value){return true} else{return 'I need a value to continue'}}
+        }
+    ])
